9db10ce310317f4f16bbf9552fc94d33a5c6075a
Anemone <x565178035@126.com>
2016-06-12 21:07:48 +0800

    Inital commit.

---
+++ class_report/test.c
@@ -0,0 +1,20 @@
+/*
+ * @file test.c
+ * @brief test
+ * @author Anemone,x565178035@126.com
+ * @version 1.0
+ * @date 2016-06-08 09:34
+ */
+
+#include <errno.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    printf("你好,世界\n");
+    return 0;
+}
+
+
